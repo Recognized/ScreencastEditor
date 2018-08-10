543b319170761ff88c32c896cfa0cffd5bf47451
Recognized <vlad9931@gmail.com>
2018-08-10 13:49:56 +0300

    Add another extension for kotlin script

--- src/main/kotlin/vladsaif/syncedit/plugin/lang/script/psi/UIScriptFileType.kt
+++ src/main/kotlin/vladsaif/syncedit/plugin/lang/script/psi/UIScriptFileType.kt
@@ -2,9 +2,10 @@ package vladsaif.syncedit.plugin.lang.script.psi
 
 import com.intellij.openapi.fileTypes.LanguageFileType
 import org.jetbrains.kotlin.idea.KotlinFileType
+import org.jetbrains.kotlin.idea.KotlinLanguage
 import javax.swing.Icon
 
-object UIScriptFileType : LanguageFileType(UIScriptLanguage) {
+object UIScriptFileType : LanguageFileType(KotlinLanguage.INSTANCE) {
   override fun getIcon(): Icon? = KotlinFileType.INSTANCE.icon
 
   override fun getName(): String = "UI Script"
--- src/main/kotlin/vladsaif/syncedit/plugin/lang/script/psi/UIScriptLanguage.kt
+++
@@ -1,5 +0,0 @@
-package vladsaif.syncedit.plugin.lang.script.psi
-
-import com.intellij.lang.Language
-
-object UIScriptLanguage : Language("GUI Script")
