6384181bd89ad2159af1a94580e41eea03e7f4e2
Recognized <vlad9931@gmail.com>
2018-10-09 12:29:39 +0300

    Add outline panel

--- src/main/kotlin/vladsaif/syncedit/plugin/sound/CountdownPanel.kt
+++ src/main/kotlin/vladsaif/syncedit/plugin/sound/CountdownPanel.kt
@@ -1,15 +1,15 @@
 package vladsaif.syncedit.plugin.sound
 
-import com.intellij.openapi.ui.AbstractPainter
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.UIUtil
 import java.awt.*
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicInteger
+import javax.swing.JPanel
 import javax.swing.Timer
 
 
-class CountDownPainter(count: Int) : AbstractPainter() {
+class CountdownPanel(count: Int) : JPanel() {
   private val myCounter = AtomicInteger(count)
   private val myTimer = Timer(1000) {
     myCounter.decrementAndGet()
@@ -17,20 +17,25 @@ class CountDownPainter(count: Int) : AbstractPainter() {
       deactivate()
       return@Timer
     }
-    setNeedsRepaint(true)
+    repaint()
   }
   private var myDeactivateNextTime = AtomicBoolean(false)
   var deactivationAction: (() -> Unit)? = null
+
+  init {
+    isVisible = true
+    isOpaque = false
+  }
 
   private fun deactivate() {
     myTimer.stop()
     deactivationAction?.invoke()
   }
 
-  override fun executePaint(component: Component?, g: Graphics2D?) {
-    component ?: return
+  override fun paint(g: Graphics?) {
+    super.paint(g)
     g ?: return
-    with(g) {
+    with(g as Graphics2D) {
       font = UIUtil.getLabelFont().deriveFont(Font.BOLD).deriveFont(JBUI.scale(100.0f))
       stroke = BasicStroke(JBUI.scale(4.0f))
       val metrics = getFontMetrics(font)
@@ -43,8 +48,8 @@ class CountDownPainter(count: Int) : AbstractPainter() {
       val shape = gv.outline
       Color.BLACK
       translate(
-          component.width / 2 - metrics.stringWidth(str) / 2,
-          component.height / 3 + metrics.height
+          width / 2 - metrics.stringWidth(str) / 2,
+          height / 3 + metrics.height
       )
       draw(shape)
       color = Color.YELLOW
@@ -53,7 +58,6 @@ class CountDownPainter(count: Int) : AbstractPainter() {
   }
 
   fun countDown() {
-    setNeedsRepaint(true)
     myTimer.start()
   }
 }
--- src/main/kotlin/vladsaif/syncedit/plugin/sound/SoundRecorder.kt
+++ src/main/kotlin/vladsaif/syncedit/plugin/sound/SoundRecorder.kt
@@ -1,15 +1,19 @@
 package vladsaif.syncedit.plugin.sound
 
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.progress.util.ProgressWindow
 import com.intellij.openapi.project.Project
+import com.intellij.util.containers.ContainerUtil
 import java.io.IOException
 import java.nio.file.Path
 import javax.sound.sampled.*
 import kotlin.concurrent.thread
+import kotlin.system.measureTimeMillis
 
 
 object SoundRecorder {
+  private val LOG = logger<SoundRecorder>()
   private val RECORD_FORMAT = AudioFormat(
       AudioFormat.Encoding.PCM_SIGNED,
       44100f,
@@ -19,22 +23,51 @@ object SoundRecorder {
       44100f,
       false
   )
-  private var activeLine: TargetDataLine? = null
+  private val LISTENERS = ContainerUtil.newConcurrentSet<StateListener>()
+  private var STATE: InternalState = InternalState.Idle
+    set(newValue) {
+      val oldValue = field
+      field = newValue
+      fireStateChanged(oldValue.toState(), newValue.toState())
+    }
+
+  init {
+    // logger listener
+    addListener(object : StateListener {
+      override fun stateChanged(oldValue: State, newValue: State) {
+        LOG.info("Changed state: $oldValue -> $newValue")
+      }
+    })
+  }
+
+  fun getState(): State {
+    ApplicationManager.getApplication().assertIsDispatchThread()
+    return STATE.toState()
+  }
 
   /**
    * @throws javax.sound.sampled.LineUnavailableException If data line cannot be acquired, or opened,
    * or if recording format is not supported.
    */
-  @Synchronized
-  fun startRecording(out: Path, project: Project, listener: SoundRecorderListener) {
+  fun start(out: Path, project: Project, errorHandler: (IOException) -> Unit) {
     ApplicationManager.getApplication().assertIsDispatchThread()
+    val state = STATE
+    when (state) {
+      is InternalState.Paused -> {
+        // Start after pause
+        state.line.start()
+        STATE = InternalState.Recording(state.line)
+        return
+      }
+      is InternalState.Recording, is InternalState.Preparing -> return
+    }
     val info = DataLine.Info(TargetDataLine::class.java, RECORD_FORMAT)
     if (!AudioSystem.isLineSupported(info)) {
       throw LineUnavailableException("Audio format ($RECORD_FORMAT) is not supported")
     }
+    val startTime = System.currentTimeMillis()
     val line = AudioSystem.getLine(info) as TargetDataLine
-    activeLine = line
-    listener.beforeRecordingStart()
+    STATE = InternalState.Preparing
     val p = ProgressWindow(false, false, project)
     p.title = "Screencast Recorder"
     p.setDelayInMillis(80)
@@ -44,29 +77,97 @@ object SoundRecorder {
       line.use {
         line.open(RECORD_FORMAT)
         line.start()
+        LOG.info("Took ${System.currentTimeMillis() - startTime}ms to start")
+        ApplicationManager.getApplication().invokeAndWait {
+          STATE = InternalState.Recording(line)
+        }
         p.stop()
         p.processFinish()
-        listener.recordingStarted()
         try {
           AudioInputStream(line).use { stream ->
             AudioSystem.write(stream, AudioFileFormat.Type.WAVE, out.toFile())
           }
         } catch (ex: IOException) {
-          listener.handleError(ex)
+          errorHandler(ex)
         } finally {
-          synchronized(SoundRecorder) {
-            activeLine = null
+          ApplicationManager.getApplication().invokeAndWait {
+            STATE = InternalState.Idle
           }
         }
       }
     }
   }
 
-  @Synchronized
-  fun stopRecording() {
+  fun pause() {
     ApplicationManager.getApplication().assertIsDispatchThread()
-    activeLine?.stop()
-    activeLine?.close()
-    activeLine = null
+    val state = STATE
+    when (state) {
+      is InternalState.Idle, is InternalState.Paused, is InternalState.Preparing -> return
+      is InternalState.Recording -> {
+        val time = measureTimeMillis {
+          state.line.stop()
+        }
+        STATE = InternalState.Paused(state.line)
+        LOG.info("Took ${time}ms to pause")
+      }
+    }
+  }
+
+  fun stop() {
+    ApplicationManager.getApplication().assertIsDispatchThread()
+    val state = STATE
+    when (state) {
+      is InternalState.Idle, is InternalState.Preparing -> return
+      is InternalState.Recording, is InternalState.Paused -> {
+        val time = measureTimeMillis {
+          if (state is InternalState.Recording) {
+            state.line.stop()
+            state.line.close()
+          } else {
+            (state as InternalState.Paused).line.close()
+          }
+        }
+        STATE = InternalState.Idle
+        LOG.info("Took ${time}ms to pause")
+      }
+    }
+  }
+
+  fun addListener(listener: StateListener) {
+    LISTENERS.add(listener)
+  }
+
+  fun removeListener(listener: StateListener) {
+    LISTENERS.remove(listener)
+  }
+
+  private fun fireStateChanged(oldValue: State, newValue: State) {
+    ApplicationManager.getApplication().assertIsDispatchThread()
+    if (oldValue == newValue) return
+    for (listener in LISTENERS) {
+      listener.stateChanged(oldValue, newValue)
+    }
+  }
+
+  private sealed class InternalState {
+    object Idle : InternalState()
+    object Preparing : InternalState()
+    class Recording(val line: TargetDataLine) : InternalState()
+    class Paused(val line: TargetDataLine) : InternalState()
+  }
+
+  private fun InternalState.toState() = when (this) {
+    is InternalState.Idle -> State.IDLE
+    is InternalState.Preparing -> State.IDLE
+    is InternalState.Paused -> State.PAUSED
+    is InternalState.Recording -> State.RECORDING
+  }
+
+  interface StateListener {
+    fun stateChanged(oldValue: State, newValue: State)
+  }
+
+  enum class State {
+    IDLE, RECORDING, PAUSED
   }
 }
